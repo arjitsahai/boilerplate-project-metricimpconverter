d9ec9ca8a909d1c618f265f395089f8cd2907fee
arjitsahai <62187739+arjitsahai@users.noreply.github.com>
2020-08-07 00:06:32 +0530

    Update api.js

--- routes/api.js
+++ routes/api.js
@@ -1,30 +1,35 @@
-/*
-*
-*
-*       Complete the API routing below
-*
-*
-*/
-
 'use strict';
 
 var expect = require('chai').expect;
 var ConvertHandler = require('../controllers/convertHandler.js');
 
-module.exports = function (app) {
-  
-  var convertHandler = new ConvertHandler();
+module.exports = function(app) {
 
-  app.route('/api/convert')
-    .get(function (req, res){
-      var input = req.query.input;
-      var initNum = convertHandler.getNum(input);
-      var initUnit = convertHandler.getUnit(input);
-      var returnNum = convertHandler.convert(initNum, initUnit);
-      var returnUnit = convertHandler.getReturnUnit(initUnit);
-      var toString = convertHandler.getString(initNum, initUnit, returnNum, returnUnit);
-      
-      //res.json
-    });
-    
+    var convertHandler = new ConvertHandler();
+
+    app.route('/api/convert')
+        .get(function(req, res) {
+            var input = req.query.input;
+            var initNum = convertHandler.getNum(input);
+            var initUnit = convertHandler.getUnit(input);
+            var returnNum = convertHandler.convert(initNum, initUnit);
+            var returnUnit = convertHandler.getReturnUnit(initUnit);
+            var toString = convertHandler.getString(initNum, initUnit, returnNum, returnUnit);
+
+            if (initNum == 'invalid number' && initUnit == 'invalid unit') {
+                res.send('invalid number and unit');
+            } else if (initNum == 'invalid number') {
+                res.send('invalid number');
+            } else if (initUnit == 'inavlid unit') {
+                res.send('invalid unit');
+            }
+            res.json({
+                "initNum": initNum,
+                "initunit": initUnit,
+                "retuenNum": returnNum,
+                "returnUnit": returnUnit,
+                "string": toString
+            })
+        });
+
 };
